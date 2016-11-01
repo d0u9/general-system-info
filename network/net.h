7701308cb0a2c70a60d0e29ad440f848e0d44173
d0u9 <d0u9.su@outlook.com>
2016-11-01 19:22:07 +0800

    network: update basic data structure

--- network/net.h
+++ network/net.h
@@ -2,35 +2,44 @@
 #define _TRI_NET_H
 
 #include <arpa/inet.h>
+#include <net/if.h>
 #include <trilib/core.h>
 #include <trilib/list.h>
+#include <trilib/bitmap.h>
+
+#define NET_DEVICE_NUM_MAX	16
 
 struct addr_desc {
+	int family;
 	struct in_addr addr4;
 	struct in6_addr addr6;
 	struct list_head addr;
 };
 
 struct dev_desc {
+	char name[IFNAMSIZ];
 	u64 mac;
 	bool online;
 	u32 mtu;
 	u16 vendor;
 	u16 device;
 	struct addr_desc addrs;
-	struct list_head list;
-	struct list_head online_list;
 };
 
 struct net {
-	u32	total_dev_num;
-	u32	online_dev_num;
-	u64	total_tx_bytes;
-	u64	total_rx_bytes;
-	u64	total_tx_packages;
-	u64	total_rx_packages;
-	struct list_head devs;
-	struct list_head online_devs;
+	u32 total_dev_num;
+	u32 online_dev_num;
+	u64 total_tx_bytes;
+	u64 total_rx_bytes;
+	u64 total_tx_packages;
+	u64 total_rx_packages;
+	struct dev_desc devs[NET_DEVICE_NUM_MAX];
+	unsigned long dev_btmp[BITS_TO_LONGS(NET_DEVICE_NUM_MAX)];
+	unsigned long online_btmp[BITS_TO_LONGS(NET_DEVICE_NUM_MAX)];
 };
+
+extern struct net *net_init(void);
+extern void net_exit(struct net *net);
+extern void net_update(struct net *net);
 
 #endif /* _TRI_NET_H */
