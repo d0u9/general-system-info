4951f3f674e2fce006bb4ad512b53d24f045d982
d0u9 <d0u9.su@outlook.com>
2016-11-03 00:36:42 +0800

    network: functional

--- network/sample/main.c
+++ network/sample/main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <net.h>
+#include <unistd.h>
 
 int main(void)
 {
 	struct net *net = NULL;
 	net = net_init();
 	net_update(net);
+	printf("tx bytes: %lu, rx bytes %lu\n", net->total_tx_bytes, net->total_rx_bytes);
+	printf("----------------------\n");
+	sleep(2);
 	net_update(net);
+	printf("tx bytes: %lu, rx bytes %lu\n", net->total_tx_bytes, net->total_rx_bytes);
+	printf("----------------------\n");
+	sleep(2);
 	net_update(net);
+	printf("tx bytes: %lu, rx bytes %lu\n", net->total_tx_bytes, net->total_rx_bytes);
 	net_exit(net);
 	return 0;
 }
