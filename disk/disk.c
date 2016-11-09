aeb69dc72ae86cee486ecdd9e7f6db808424cb74
d0u9 <d0u9.su@outlook.com>
2016-11-09 12:42:04 +0800

    disk: add test makefile and sample dir

---
+++ disk/disk.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include <disk.h>
+#include <trilib/list.h>
+
+
+
---
+++ disk/sample/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <disk.h>
+
+int main(void)
+{
+	printf("hello world!\n");
+	return 0;
+}
