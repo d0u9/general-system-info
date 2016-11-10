ab61f3cdf7ed3d35687478ae88db847232d654b5
d0u9 <d0u9.su@outlook.com>
2016-11-10 15:33:10 +0800

    disk: basic program structure

--- disk/disk.c
+++ disk/disk.c
@@ -3,5 +3,20 @@
 #include <disk.h>
 #include <trilib/list.h>
 
+struct disks *disk_init(void)
+{
+
+	return NULL;
+}
+
+void disk_update(struct disks *disk_root)
+{
+
+}
+
+void cpu_exit(struct disks *disk_root)
+{
+
+}
 
 
