b2590474ee7f632a52f7575232faa7334b717a5b
d0u9 <d0u9.su@outlook.com>
2016-11-14 09:33:07 +0800

    disk: update data structures

--- disk/disk.h
+++ disk/disk.h
@@ -5,6 +5,7 @@
 #include <trilib/list.h>
 
 #define MODEL_NAME_LEN_MAX	128
+#define DEV_NAME_LEN_MAX	16
 
 struct io_desc {
 	u64 read_io;
@@ -16,13 +17,13 @@ struct io_desc {
 };
 
 struct disk_desc {
+	unsigned devname[DEV_NAME_LEN_MAX];
 	unsigned long size;				//in 512bytes sectors
 	char model[MODEL_NAME_LEN_MAX];
 	struct io_desc io;
-	struct list_head disks;
-	struct list_head partitions;
+	struct list_head disk_list;
+	struct list_head d_partitions;
 };
-
 
 struct partition_desc {
 	unsigned long bsize;
@@ -38,16 +39,19 @@ struct partition_desc {
 	unsigned long namemax;
 	struct io_desc io;
 	struct disk_desc *p_disk;
+	struct list_head d_partitions;
 	struct list_head partitions;
-	struct list_head all_partitions;
 };
-
 
 struct disks {
 	u64 total_read;
 	u64 total_write;
-	struct list_head disks;
-	struct list_head all_partitions;
+	struct list_head disk_list;
+	struct list_head partitions;
 };
+
+extern struct disks *disk_init(void);
+extern void disk_update(struct disks *disk_root);
+extern void disk_exit(struct disks *disk_root);
 
 #endif /* _TRI_DISK_H */
