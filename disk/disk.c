9d8b6b0cdb47c04f6fa95675e1bf6f96632b493a
d0u9 <d0u9.su@outlook.com>
2016-11-15 12:31:25 +0800

    disk: update

--- disk/disk.c
+++ disk/disk.c
@@ -1,20 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
 
 #include <disk.h>
 #include <trilib/list.h>
 
+#define SYSFS_DIR	"/sys/block"
+
+void scan_sysfs(struct disks *disk_root, struct list_head *tmp_disk_list,
+		struct list_head *tmp_partitions)
+{
+	DIR *sys_dir = NULL;
+	struct dirent *dirent = NULL;
+
+	sys_dir = opendir(SYSFS_DIR);
+
+	while((dirent = readdir(sys_dir))) {
+		if(!strcmp(".", dirent->d_name) || !strcmp("..", dirent->d_name))
+			continue;
+
+		printf("%s\n", dirent->d_name);
+	}
+
+}
+
 struct disks *disk_init(void)
 {
-	return calloc(1, sizeof(struct disks));
+	struct disks *ret = NULL;
+	ret = (struct disks*)calloc(1, sizeof(struct disks));
+	INIT_LIST_HEAD(&ret->disk_list);
+	INIT_LIST_HEAD(&ret->partitions);
+
+	return ret;
 }
 
 void disk_update(struct disks *disk_root)
 {
+	LIST_HEAD(tmp_disk_list);
+	LIST_HEAD(tmp_partitions);
+	/*list_cut_position(&tmp_disk_list, &disk_root->disk_list, (&disk_root->disk_list)->prev);*/
+	list_splice_init(&disk_root->partitions, &tmp_partitions);
+
+
+	scan_sysfs(disk_root, &tmp_disk_list, &tmp_partitions);
 
 }
 
-void cpu_exit(struct disks *disk_root)
+void disk_exit(struct disks *disk_root)
 {
 
 }
--- disk/sample/main.c
+++ disk/sample/main.c
@@ -3,6 +3,24 @@
 
 int main(void)
 {
+	struct disks *disk_root = NULL;
+
 	printf("hello world!\n");
+	printf("---------------------------------------\n");
+
+	disk_root = disk_init();
+
+	printf("---------------------------------------\n");
+	disk_update(disk_root);
+
+	printf("---------------------------------------\n");
+	disk_update(disk_root);
+
+	printf("---------------------------------------\n");
+	disk_update(disk_root);
+
+	printf("---------------------------------------\n");
+	disk_exit(disk_root);
+
 	return 0;
 }
