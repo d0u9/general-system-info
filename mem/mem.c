d5785e2b2e0ae12ead2c9d38281f0949ff274c5b
d0u9 <d0u9.su@outlook.com>
2016-11-28 09:16:34 +0800

    memory: create mem.c

---
+++ mem/mem.c
@@ -0,0 +1,18 @@
+#include <utils.h>
+#include <trilib/log.h>
+#include <mem.h>
+
+struct mem_desc *mem_init(void)
+{
+	return NULL;
+}
+
+void mem_update(struct mem_desc *mem)
+{
+
+}
+
+void mem_exit(struct mem_desc mem)
+{
+
+}
